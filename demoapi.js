5e5f066731eebc8030eeebb0a5b712267a383d1a
Jimmy Muchachasoy <test>
2022-04-27 11:20:47 -0500

    version initial

---
+++ demoapi.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require("body-parser");
+
+const app = express()
+const port = 3000
+var statedevice ="Sleep"
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req,res) =>{
+    res.send('estate device!'+statedevice)
+})
+
+app.post('/statedevice',(req,res)=>{
+    statedevice = req.body.state
+    console.log("POST estate device " +statedevice)
+    res.send('POST estate device!'+statedevice)
+})
+app.listen(port,()=>{
+    console.log('Ejecutando app desde el puerto ${port}')
+})
